515f02576e62fb23a01228c455f9e0b27b19f036
Michael Graham <gra3@pm.me>
2019-12-14 23:43:50 -0600

    http request parsed

--- js/muhplot.js
+++ js/muhplot.js
@@ -1,4 +1,24 @@
+// GET JSON FROM HTTP REQUEST
+var theUrl = "https://api.nasa.gov/insight_weather/?api_key=c3RFS78h4rzwJOM0y6fSoo4CBWgiIw32DajxPtpa&feedtype=json&ver=1.0";
+var JSO;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', theUrl, true);
+xhr.send();
+xhr.addEventListener("readystatechange", processRequest, false);
 
+function processRequest(e) {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+		JSO = JSON.parse(xhr.responseText);
+    }
+}
+
+
+var graphDiv;
+var data;
+var layout;
+function setData(){
+
+}
 /* TEMPERATURE */
 var graphDiv = document.getElementById('temperature-plot');
 var data = [{
@@ -6,10 +26,10 @@ var data = [{
 	y: [1, 2, 4, 8, 6, 2, 8, 2, 6, 82, 13, 32, 22, 12, 55, 23] ,
 	type: 'scatter',
 	marker: {
-		color: '#dfdfdf'
+		color: '#8cd5ff'
 	  },
 	line: {
-		color: '#dfdfdf'
+		color: '#d1ff82'
 	}
 }];
 
@@ -53,10 +73,10 @@ data = [{
 	y: [1, 2, 4, 8, 6, 2, 8, 2, 6, 82, 13, 32, 22, 12, 55, 23] ,
 	type: 'scatter',
 	marker: {
-		color: '#dfdfdf'
+		color: '#8cd5ff'
 	},
 	line: {
-		color: '#dfdfdf'
+		color: '#d1ff82'
 	},
 	/*type: 'bar'*/
 }];
@@ -87,3 +107,63 @@ layout = {
 	plot_fgcolor: "rgba(223,223,223,1)"
 };
 Plotly.newPlot(graphDiv, data, layout);
+
+
+data = [{
+    r: [77.5, 72.5, 70.0, 45.0, 22.5, 42.5, 40.0, 62.5],
+    theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
+    name: "11-14 m/s",
+    marker: {color: "rgb(106,81,163)"},
+    type: "barpolar"
+  }, {
+    r: [57.5, 50.0, 45.0, 35.0, 20.0, 22.5, 37.5, 55.0],
+    theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
+    name: "8-11 m/s",
+    marker: {color: "rgb(158,154,200)"},
+    type: "barpolar"
+  }, {
+    r: [40.0, 30.0, 30.0, 35.0, 7.5, 7.5, 32.5, 40.0],
+    theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
+    name: "5-8 m/s",
+    marker: {color: "rgb(203,201,226)"},
+    type: "barpolar"
+  }, {
+    r: [20.0, 7.5, 15.0, 22.5, 2.5, 2.5, 12.5, 22.5],
+    theta: ["North", "N-E", "East", "S-E", "South", "S-W", "West", "N-W"],
+    name: "< 5 m/s",
+    marker: {color: "rgb(242,240,247)"},
+    type: "barpolar"
+  }]
+layout = {
+    font: {size: 16},
+	legend: {font: {size: 16}},
+	color: '#dfdfdf',
+    polar: {
+      barmode: "overlay",
+      bargap: 0,
+      radialaxis: {ticksuffix: "%", angle: 45, dtick: 20},
+      angularaxis: {direction: "clockwise"}
+	},
+	title: {
+		text:'Wind Direction [Wind Rose Chart]: x days',
+		font: {
+		  family: 'Courier New, monospace',
+		  size: 24,
+		  color: '#dfdfdf'
+		}
+	},
+
+	paper_bgcolor: "rgba(255,255,255,0)",
+	plot_bgcolor: "rgba(255,255,255,.1)",
+	paper_fgcolor: "rgba(223,223,223,1)",
+	plot_fgcolor: "rgba(223,223,223,1)"
+  }
+
+graphDiv = document.getElementById('wind-direction-plot');
+Plotly.newPlot(graphDiv, data, layout)
+
+
+
+
+
+
