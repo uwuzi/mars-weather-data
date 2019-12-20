b0ab1f7a2e585bd7a94900d12c73a0ff69817864
Michael Graham <gra3@pm.me>
2019-12-19 23:32:18 -0600

    Cleaned up c++ file. This is my first javascript project damnit, and I want github to show that little yellow dot next to the repository

--- src/request.cpp
+++ src/request.cpp
@@ -2,15 +2,10 @@
 #include <fstream>
 #include <string>
 #include <curl/curl.h>
-#include "json.hpp"
+#include <nlohmann/json.hpp>
 #include "sol.hpp"
 
 using namespace nlohmann;
-
-bool parseCmdArgs(int _argc, char* _argv[])
-{
-    return true;
-}
 
 std::string readApiKey()
 {
@@ -56,49 +51,25 @@ std::string makeRequest(std::string httpsUrl)
 #define SKIP_HOSTNAME_VERIFICATION 0
  
 #ifdef SKIP_PEER_VERIFICATION
-    /*
-     * If you want to connect to a site who isn't using a certificate that is
-     * signed by one of the certs in the CA bundle you have, you can skip the
-     * verification of the server's certificate. This makes the connection
-     * A LOT LESS SECURE.
-     *
-     * If you have a CA cert for the server stored someplace else than in the
-     * default bundle, then the CURLOPT_CAPATH option might come handy for
-     * you.
-     */ 
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 #else
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
 #endif
  
 #ifdef SKIP_HOSTNAME_VERIFICATION
-    /*
-     * If the site you're connecting to uses a different host name that what
-     * they have mentioned in their server certificate's commonName (or
-     * subjectAltName) fields, libcurl will refuse to connect. You can skip
-     * this check, but this will make the connection less secure.
-     */ 
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 #else
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1L);
 #endif
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlCallbackFuncToString);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-    //curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); // verbose output
- 
-    /* Perform the request, res will get the return code */ 
     res = curl_easy_perform(curl);
-    /* Check for errors */ 
     if(res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
- 
-    /* always cleanup */ 
     curl_easy_cleanup(curl);
   }
- 
   curl_global_cleanup();
-
   return s;
 }
 
@@ -164,8 +135,6 @@ int jsonCat(nlohmann::json& dest, const nlohmann::json& src, const std::string& 
     return -1;
 }
 
-
-
 std::string resolveRequestURL() {
     return "https://api.nasa.gov/insight_weather/?api_key=" + readApiKey() + "&feedtype=json&ver=1.0";
 }
@@ -179,7 +148,6 @@ nlohmann::json updateJson(const char* oldJsonFile, nlohmann::json& requestJSO, s
     return readJSO;
 }
 
-
 int main (int argc, char* argv[]) 
 {
     const char* jsonFilePath = "../data/data.json";
@@ -187,52 +155,6 @@ int main (int argc, char* argv[])
     std::vector<std::string> sol = requestJSO["sol_keys"];
     std::cout << updateJson(jsonFilePath, requestJSO, sol);
     return 0;
-
-#define PROCESS_DATA 0
-#if PROCESS_DATA
-    unsigned short requestCount;
-    std::vector<Sol*> solVec;
-    for (int i = 0; i < sol.size(); i++) {
-        Sol *tmp = new Sol();
-        std::string key = sol.at(i);
-        tmp->setSolKey(key);
-        tmp->setTemperatureData(
-            JSO[key]["AT"]["av"],
-            JSO[key]["AT"]["ct"],
-            JSO[key]["AT"]["mn"],
-            JSO[key]["AT"]["mx"]
-        );
-        tmp->setWindData(
-            JSO[key]["HWS"]["av"],
-            JSO[key]["HWS"]["ct"],
-            JSO[key]["HWS"]["mn"],
-            JSO[key]["HWS"]["mx"]
-        );
-        tmp->setWindDirectionData(
-            JSO[key]["WD"]["most_common"]["compass_degrees"],
-            JSO[key]["WD"]["most_common"]["compass_point"],
-            JSO[key]["WD"]["most_common"]["compass_right"],
-            JSO[key]["WD"]["most_common"]["compass_up"],
-            JSO[key]["WD"]["most_common"]["ct"]
-        );
-        tmp->setPressureData(
-            JSO[key]["PRE"]["av"],
-            JSO[key]["PRE"]["ct"],
-            JSO[key]["PRE"]["mn"],
-            JSO[key]["PRE"]["mx"]
-        );
-        tmp->setTimeData(
-            JSO[key]["First_UTC"],
-            JSO[key]["Last_UTC"],
-            JSO[key]["Season"]
-        );
-
-        std::cout << std::endl;
-        tmp->print(key);
-        solVec.push_back(tmp);
-    }
-    return 0;
-#endif
 }
 
 
