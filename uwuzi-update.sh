fad6838407d76c69b3bd859407f45122e9a90bf8
uwuzi <55362709+uwuzi@users.noreply.github.com>
2019-12-23 22:43:01 +0000

    Update uwuzi-update.sh

--- uwuzi-update.sh
+++ uwuzi-update.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+baseLocation="/path/to/base"
+backupLocation="/path/to/backup/directory"
+logLocation="/path/to/log"
 
 # Backup old json, replace with new json
 backupName="data$(date +%Y-%m-%d-%H:%M:%S).json"
 
-cd /data/uwuzi.com/src
-/data/uwuzi.com/src/updatejson > /data/uwuzi.com/src/new.json
+cd "$baseLocation"
+$baseLocation/updatejson > $baseLocation/new.json
 printf "New JSON generated\n\n"
 
-cp -v /data/uwuzi.com/data/data.json /data/uwuzi.com/backup/"$backupName"
-printf "Backup of old JSON stored at /data/uwuzi.com/backup/$backupName\n\n"
+cp -v $backupLocation/data.json $backupLocation/"$backupName"
+printf "Backup of old JSON stored at $backupLocation/$backupName\n\n"
 
-mv -v /data/uwuzi.com/src/new.json /data/uwuzi.com/data/data.json
+mv -v $baseLocation/new.json $backupLocation/data.json
 printf "New JSON moved into data directory\n\n"
 
 # Log the event
-echo "$(date +%Y-%m-%d-%H:%M:%S) -- $backupName" >> /var/log/nginx/uwuzi-backup.log
-printf "Log written to /var/log/nginx/uwuzi-backup.log\n\n"
+echo "$(date +%Y-%m-%d-%H:%M:%S) -- $backupName" >> $logLocation/uwuzi-backup.log
+printf "Log written to $logLocation/uwuzi-backup.log\n\n"
