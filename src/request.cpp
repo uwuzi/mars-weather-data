89cd10767a94f4f7228a880f98a78d3df04ae617
Michael Graham <gra3@pm.me>
2019-12-24 13:39:06 -0600

    Added validity check before merging newest data

--- src/request.cpp
+++ src/request.cpp
@@ -2,10 +2,15 @@
 #include <fstream>
 #include <string>
 #include <curl/curl.h>
-#include <nlohmann/json.hpp>
+#include "json.hpp"
 #include "sol.hpp"
 
 using namespace nlohmann;
+
+bool parseCmdArgs(int _argc, char* _argv[])
+{
+    return true;
+}
 
 std::string readApiKey()
 {
@@ -51,25 +56,49 @@ std::string makeRequest(std::string httpsUrl)
 #define SKIP_HOSTNAME_VERIFICATION 0
  
 #ifdef SKIP_PEER_VERIFICATION
+    /*
+     * If you want to connect to a site who isn't using a certificate that is
+     * signed by one of the certs in the CA bundle you have, you can skip the
+     * verification of the server's certificate. This makes the connection
+     * A LOT LESS SECURE.
+     *
+     * If you have a CA cert for the server stored someplace else than in the
+     * default bundle, then the CURLOPT_CAPATH option might come handy for
+     * you.
+     */ 
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 #else
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
 #endif
  
 #ifdef SKIP_HOSTNAME_VERIFICATION
+    /*
+     * If the site you're connecting to uses a different host name that what
+     * they have mentioned in their server certificate's commonName (or
+     * subjectAltName) fields, libcurl will refuse to connect. You can skip
+     * this check, but this will make the connection less secure.
+     */ 
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
 #else
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1L);
 #endif
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlCallbackFuncToString);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+    //curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); // verbose output
+ 
+    /* Perform the request, res will get the return code */ 
     res = curl_easy_perform(curl);
+    /* Check for errors */ 
     if(res != CURLE_OK)
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
+ 
+    /* always cleanup */ 
     curl_easy_cleanup(curl);
   }
+ 
   curl_global_cleanup();
+
   return s;
 }
 
@@ -135,16 +164,33 @@ int jsonCat(nlohmann::json& dest, const nlohmann::json& src, const std::string& 
     return -1;
 }
 
+
+
 std::string resolveRequestURL() {
     return "https://api.nasa.gov/insight_weather/?api_key=" + readApiKey() + "&feedtype=json&ver=1.0";
 }
 
+bool isValidData(json& JSO, std::vector<std::string>& solVec)
+{
+	std::string solNum = solVec.back();
+	if (JSO[solNum]["AT"].is_null() || JSO[solNum]["HWS"].is_null()||
+		JSO[solNum]["PRE"].is_null() || JSO[solNum]["WD"].is_null()) {
+			return false;
+	}
+	return true;
+}
+
 nlohmann::json updateJson(const char* oldJsonFile, nlohmann::json& requestJSO, std::vector<std::string>& sol) {
+
     nlohmann::json readJSO;
     jsonRead(readJSO, oldJsonFile);
-    json newJson;
-    newJson[sol.back()] = requestJSO[sol.back()];
-    jsonCat(readJSO,newJson,sol.back());
+
+	if (isValidData(requestJSO, sol)) {
+    	json newJson;
+    	newJson[sol.back()] = requestJSO[sol.back()];
+    	jsonCat(readJSO,newJson,sol.back());
+	}
+
     return readJSO;
 }
 
@@ -156,9 +202,3 @@ int main (int argc, char* argv[])
     std::cout << updateJson(jsonFilePath, requestJSO, sol);
     return 0;
 }
-
-
-
-
-
-
