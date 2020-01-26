0f70dbf9b0d6ca260c8b0a6235519d99fce3c41e
Michael Graham <gra3@pm.me>
2020-01-25 21:35:07 -0600

    Updated backup script

--- uwuzi-backup.sh
+++ uwuzi-backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 connection="$(ping -q -w 1 -c 1 www.uwuzi.com > /dev/null && echo ok || echo error)"
-location=~/Programming/"data-$(date --iso-8601).json";
+location=~/Code/uwuzi-data-backup/"data$(date +%Y-%m-%d-%H:%M:%S).json";
 
-if [ -f "$location" ]; then
-    notify-send "FILE EXISTS" uwuzi-backup;
-elif [ $connection = "ok" ]; then
+if [ $connection = "error" ]; then
+    notify-send FAILURE uwuzi-backup;
+#elif [ -f "$location" ]; then
+#	notify-send "FILE EXISTS" uwuzi-backup;
+else
 	curl http://www.uwuzi.com/data/data.json > "$location";
     notify-send SUCCESS uwuzi-backup;
-	firefox -new-window "$location";
-else
-    notify-send FAILURE uwuzi-backup;
+	waterfox-classic -new-window "$location";
 fi
 
